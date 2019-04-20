5dc65f3a2231c50b7dbf65327173cc7b3d4e7046
Joao Marcos <joaomarcos1999jm@gmail.com>
2019-04-20 12:19:58 -0300

    Correção no modulo de tratamento de texto. Junção da função de tratamento em uma só

--- src/word_treatment.cpp
+++ src/word_treatment.cpp
@@ -4,30 +4,21 @@ std::vector <std::string> split(std::string& phrase, std::string separator) {
 
 }
 
-void to_lowercase(std::string& word) {
+void treat(std::string& word) {
     for(int i=0; i<word.length(); i++) {
         char c = word[i];
+
+        // Remove special char
+        if(!isalnum(c)) {
+            word.erase(i, 1);
+        }
+
+        // Remove accent
+        // [...]
+
+        // Transform to lowercase
         if(isupper(c)) {
             word[i] = tolower(c);
         }
     }
-}
-
-void remove_special_char(std::string& text) {
-    for(int i=0; i<text.length(); i++) {
-        char c = text[i];
-        if(!isalnum(c)) {
-            text.erase(i, 1);
-        }
-    }
-}
-
-void remove_accent(std::string& text) {
-
-}
-
-void treat(std::string& word) {
-    to_lowercase(word);
-    remove_accent(word);
-    remove_special_char(word);
 }
--- src/word_treatment.h
+++ src/word_treatment.h
@@ -7,16 +7,7 @@
 // Função que separa frase em uma lista de substrings
 std::vector <std::string> split(std::string& phrase, std::string separator);
 
-// Função que transforma string para lowercase
-void to_lowercase(std::string& word);
-
-// Função que remove caracteres especiais da string
-void remove_special_char(std::string& text);
-
-// Função que remove acentos dos caracteres no texto
-void remove_accent(std::string& text);
-
-// Função que trata palavra (remove acento, caracteres especiais e transforma para lowercase)
+// Função que trata palavra: remove acento, caracteres especiais e transforma para lowercase
 void treat(std::string& word);
 
 #endif
