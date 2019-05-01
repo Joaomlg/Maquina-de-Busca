2703ad400744a0db0ab21bc89a77eaf92703566d
Pierre Victor <pierre.sousa@ijunior.com.br>
2019-04-30 22:50:39 -0300

    Tirei comentarios do main

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,16 @@
 #include <fstream>
 #include <dirent.h>
 #include <string>
-/*
+
 #include "word_treatment.h"
 #include "Vocabulary.h"
 #include "List.h"
-*/
 #include "list_database.h"
 
 using namespace std;
 
 int main() {
-    /*
+
     Vocabulary vocabulary;
 
     
@@ -49,9 +48,6 @@ int main() {
         treat(query_words[i]);
         query_vocabulary.insert(query_words[i], "");
     }
-    */
-
-   requestArchievs();
 
     return 0;
 }
