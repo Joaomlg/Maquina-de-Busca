624c8b0367ff1842eb46c7483e5e6fc76cf99329
Joao Marcos <joaomarcos1999jm@gmail.com>
2019-05-04 15:10:50 -0300

    Implementação provisória do multiset no header

--- src/multiset.h
+++ src/multiset.h
@@ -7,7 +7,13 @@ template <class type>
 class Multiset : public std::multiset<type> {
     public:
         // Retorna numero de elementos diferentes no conjunto
-        int count_different() const;
+        int count_different() const {
+            std::set<type> aux;
+            for(typename std::multiset<type>::iterator it=this->begin(); it!=this->end(); it++) {
+                aux.insert(*it);
+            }
+            return aux.size();
+        }
 };
 
 #endif
