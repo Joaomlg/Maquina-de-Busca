03a327cbc73097fb343bfd56a6725dc15a1da635
Pierre Victor <pierre.sousa@ijunior.com.br>
2019-05-04 23:01:51 -0300

    Listagem de arquivos na base de dados
    Implementada com vector, falta alterar para usar o tipo lista implementada por nos mesmos ou usar a biblioteca padrao

--- src/list_database.h
+++ src/list_database.h
@@ -1,11 +1,11 @@
 #ifndef LIST_DATABASE_H
 #define LIST_DATABASE_H
 
-#include <string>
+#include <vector>
 
 using namespace std;
 
 //Organiza em uma lista, todos os arquivos do diretorio database
-string requestArchievs();
+vector <string> requestArchievs();
 
 #endif
