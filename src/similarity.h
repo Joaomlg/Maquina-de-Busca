cb52e95d33b8d108a7fcb466d06ff62d4027b0e2
Pierre Victor da Silva Sousa <pierre.sousa@ijunior.com.br>
2019-05-21 14:16:51 -0300

    Contrato para similiaridade

--- src/similarity.h
+++ src/similarity.h
@@ -4,11 +4,19 @@
 #include <string>
 #include <map>
 #include "vetor.h"
+#include <vector>
+#include <list>
 
 using namespace std;
 
+//Calcula a similiaridade entre dois vetores
 float similiaridade(Vetor doc, Vetor query);
 
+//Insere Documento e Similiaridade em um map<string,float>
+void inserir(map<string, float> &docs_simi, string document, float sim);
 
+//Ordena os documentos de um map de acordo com sua similiaridade
+//Retorna uma list de string
+list <string> order(map<string,float> docs_simi);
 
 #endif
