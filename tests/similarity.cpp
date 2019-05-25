dc26305c97d6224d30773da2c0ef56077e6f4476
Pierre Victor da Silva Sousa <pierre.sousa@ijunior.com.br>
2019-05-25 18:05:19 -0300

    Documento de teste para similiaridade

---
+++ tests/similarity.cpp
@@ -0,0 +1,11 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "../include/doctest.h"
+
+#include "../src/similarity.h"
+
+using namespace std;
+
+TEST_CASE("similiaridade(Vetor doc, Vetor query)") {
+    
+}
