56e673c61e4fea33d876956719e2931b7265a4ba
Pierre Victor <pierre.sousa@ijunior.com.br>
2019-06-09 15:31:51 -0300

    Listar database Recusivamente

--- src/list_database.cpp
+++ src/list_database.cpp
@@ -11,25 +11,34 @@ vector <string> requestArchievs(string pasta){
 
     DIR *diretorio;
     struct dirent *lsdir;
-    string arquivo;
+    string arquivo, newDir, dirSum;
     vector <string> arquivos, aux;
 
     diretorio = opendir(pasta.c_str());
+     
+    if (diretorio == 0) {
+        std::cerr << "Nao foi possivel abrir diretorio." << std::endl;
+        exit (1);
+    }
 
     while ( ( lsdir = readdir(diretorio) ) != NULL )
     {
-        if( lsdir->d_type == DT_REG ){
-            arquivos.push_back(lsdir->d_name);
-        } else{
-            aux = requestArchievs(lsdir->d_name);
-            for(int i=0; i<aux.size(); i++){
-                arquivos.push_back(aux[i].c_str);
+        if( lsdir->d_type == DT_DIR ){
+            arquivo = lsdir->d_name;
+            if(arquivo.size()>2){
+                newDir = pasta+"/"+arquivo;
+                aux = requestArchievs(newDir);
+                for(int i=0; i<aux.size(); i++){
+                    arquivos.push_back(aux[i]);
+                }
             }
+        } else{
+            arquivos.push_back(lsdir->d_name);
         }
-        
+
     }
-
+    
     closedir(diretorio);
-
+    
     return arquivos;
 }
--- tests/list_database.cpp
+++ tests/list_database.cpp
@@ -4,18 +4,21 @@
 
 #include "../src/list_database.h"
 
-using namespace std;
-
 TEST_CASE("requestArchievs()") {
-    string pasta = "../tests/database";
-    vector <string> expected = {"baseTeste1.txt", "baseTeste2.txt", "baseTeste3.txt"};
+    string pasta = "../database";
+    vector <string> expected = {"baseTeste1.txt", "baseTeste2.txt", "baseTeste3.txt", "doc1.txt", "doc2.txt", "doc3.txt", "d1.txt", "d2.txt", "d3.txt"};
     int checar=0;
     vector <string> received = requestArchievs(pasta);
+    
     for(int i=0; i<expected.size(); i++){
         for(int j=0; j<received.size(); j++){
-            if(expected[i]==received[j])
+            if(expected[i]==received[j]){
+                std::cerr << received[j] << std::endl;
+                CHECK(expected[i]==received[j]);
                 checar++;
+            }
         }    
     }
+    
     CHECK(checar==expected.size());
 }
