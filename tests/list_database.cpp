0218f6513de5ee5c8c44fe1674f5044a5320975a
Joao Marcos <joaomarcos1999jm@gmail.com>
2019-06-12 18:12:49 -0300

    Correção teste list_database

--- tests/list_database.cpp
+++ tests/list_database.cpp
@@ -3,10 +3,11 @@
 #include "../include/doctest.h"
 
 #include "../src/list_database.h"
+#include <set>
 
 TEST_CASE("requestArchievs()") {
     string pasta = "tests/database";
-    vector <string> expected = {
+    set <string> expected = {
         "tests/database/Subdatabase/doc3.txt", 
         "tests/database/Subdatabase/doc2.txt", 
         "tests/database/Subdatabase/dir/doc4.txt",
@@ -17,7 +18,7 @@ TEST_CASE("requestArchievs()") {
     
     REQUIRE(received.size() == expected.size());
 
-    for(int i=0, j=0; i<expected.size(); i++, j++){
-        CHECK(received[i] == expected[j]);
+    for(auto &f: received){
+        CHECK(expected.find(f) != expected.end());
     }
 }
